6fada31541b87d510d9ae9e5e07c84d82339bad7
Dilkash <dilkashpeshimam@gmail.com>
2022-10-09 15:25:45 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -10,14 +10,34 @@ app.use(cors())
 //creating http server of our app
 const server=http.createServer(app)
 
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello world',
+    });
+  });
+
 
 //Creating instance of socket.io and conecting to our server
 const io=new Server(server,{
     cors:{
         origin:'https://localhost:3000',
-        methods:["GET","POST"]
+        methods:["GET","POST"],
     }
 })
+
+io.on('connection', (socket) => {
+    console.log(`User connected ${socket.id}`);
+  
+    // We can write our socket event listeners  here...
+  
+    socket.on("join_room",(data)=>{
+        socket.join(data)
+    })
+    socket.on("send_message",(data)=>{
+        // socket.broadcast.emit("receive_message", data)
+        socket.to(data.room).emit("receive_message", data)
+    })
+  });
 
 server.listen(3001,()=>{
     console.log('SERVER RUNNING')
